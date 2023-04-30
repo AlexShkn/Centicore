211c6a22e55a72580129acf7b27d06cda38afe5d
AlexShkn <barsuk-b@yandex.ru>
2023-04-30 15:41:13 +0300

    go to top button

--- src/js/components/Page/goToTopButton.js
+++ src/js/components/Page/goToTopButton.js
@@ -4,21 +4,26 @@ const backToTopButton = document.querySelector('#back-to-top-btn')
 
 if (backToTopButton) {
 	document.addEventListener('scroll', throttle(scrollFunction, 64))
+	document.addEventListener('resize', throttle(scrollFunction, 64))
 
 	function scrollFunction() {
-		if (window.pageYOffset > 300) {
-			if (!backToTopButton.classList.contains('btnEntrance')) {
-				backToTopButton.classList.remove('btnExit')
-				backToTopButton.classList.add('btnEntrance')
-				backToTopButton.style.display = 'inline-flex'
-			}
+		if (window.innerWidth < 768) {
+			backToTopButton.style.display = 'none'
 		} else {
-			if (backToTopButton.classList.contains('btnEntrance')) {
-				backToTopButton.classList.remove('btnEntrance')
-				backToTopButton.classList.add('btnExit')
-				setTimeout(function () {
-					backToTopButton.style.display = 'none'
-				}, 250)
+			if (window.pageYOffset > 300) {
+				if (!backToTopButton.classList.contains('btnEntrance')) {
+					backToTopButton.classList.remove('btnExit')
+					backToTopButton.classList.add('btnEntrance')
+					backToTopButton.style.display = 'inline-flex'
+				}
+			} else {
+				if (backToTopButton.classList.contains('btnEntrance')) {
+					backToTopButton.classList.remove('btnEntrance')
+					backToTopButton.classList.add('btnExit')
+					setTimeout(function () {
+						backToTopButton.style.display = 'none'
+					}, 250)
+				}
 			}
 		}
 	}
@@ -26,35 +31,5 @@ if (backToTopButton) {
 	backToTopButton.addEventListener('click', e => {
 		e.preventDefault()
 		window.scrollTo(0, 0)
-		// smoothScrollBackToTop()
 	})
-
-	// Задержка перед скроллом на верх
-
-	// function smoothScrollBackToTop() {
-	// 	const targetPosition = 0
-	// 	const startPosition = window.pageYOffset
-	// 	const distance = targetPosition - startPosition
-	// 	const duration = 750
-	// 	let start = null
-
-	// 	window.requestAnimationFrame(step)
-
-	// 	function step(timestamp) {
-	// 		if (!start) start = timestamp
-	// 		const progress = timestamp - start
-	// 		window.scrollTo(
-	// 			0,
-	// 			easeInOutCubic(progress, startPosition, distance, duration),
-	// 		)
-	// 		if (progress < duration) window.requestAnimationFrame(step)
-	// 	}
-	// }
-
-	// function easeInOutCubic(t, b, c, d) {
-	// 	t /= d / 2
-	// 	if (t < 1) return (c / 2) * t * t * t + b
-	// 	t -= 2
-	// 	return (c / 2) * (t * t * t + 2) + b
-	// }
 }
